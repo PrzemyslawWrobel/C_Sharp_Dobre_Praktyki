d81eef25e59a2564075a1f696fa1404cbf11b240
PW <przemek.wrobel@poczta.fm>
2020-03-11 14:34:30 +0100

    Dodanie Class Produkt

---
+++ DevHobbyApp/DevHobby.BLL/Produkt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevHobby.BLL
+{
+
+    /// <summary>
+    /// Zarządza produktami
+    /// </summary>
+    public class Produkt
+    {
+        #region właściwości klasy
+        private int produktId;
+        public int ProduktId
+        {
+            get { return produktId; }
+            set { produktId = value; }
+        }
+        private string nazwaProduktu;
+        public string NazwaProduktu
+        {
+            get { return nazwaProduktu; }
+            set { nazwaProduktu = value; }
+        }
+        private string opis;
+
+        public string Opis
+        {
+            get { return opis; }
+            set { opis = value; }
+        }
+        #endregion
+
+        public string PowiedzWitaj()
+        {
+            return "Witaj " + NazwaProduktu + "(" + ProduktId + ")" + Opis;
+        }
+    }
+}
