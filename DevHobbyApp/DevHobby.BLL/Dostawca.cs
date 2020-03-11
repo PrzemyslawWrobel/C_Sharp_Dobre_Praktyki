8ea28a6d4dc6beb9c2fe42645c6e8d74614162d4
PW <przemek.wrobel@poczta.fm>
2020-03-11 09:49:52 +0100

    Wysyłanie email do dostawcy

---
+++ DevHobbyApp/DevHobby.BLL/Dostawca.cs
@@ -0,0 +1,31 @@
+﻿using DevHobby.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevHobby.BLL
+{
+    public class Dostawca
+    {
+        public int DostawcaId { get; set; }
+        public string NazwaFirmy { get; set; }
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Wysyła wiadomość email, aby powitać nowego dostawcę
+        /// </summary>
+        /// <param name="wiadomosc"></param>
+        /// <returns></returns>
+        public string wyslijEmailWitamy(string wiadomosc)
+        {
+            var emailService = new EmailService();
+            var temat = "Witaj " + this.NazwaFirmy;
+            var potwierdzenie = emailService.WyslijWiadomosc(temat, wiadomosc, this.Email);
+
+            return potwierdzenie;
+        }
+
+    }
+}
---
+++ DevHobbyApp/DevHobby.Common/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevHobby.Common
+{
+    public class EmailService
+    {
+        /// <summary>
+        /// Wysyła wiadomość email
+        /// </summary>
+        /// <param name="temat"></param>
+        /// <param name="wiadomosc"> tekst wiadomośći do klienta</param>
+        /// <param name="odbiorca"> adres email odbiorcy wiadomości</param>
+        /// <returns></returns>
+        public string WyslijWiadomosc(string temat, string wiadomosc, string odbiorca)
+        {
+            // kod, aby wysłać wiadomość email 
+
+            var potwierdzenie = "Wiadomość wysłąna" + temat;
+            var logowanieService = new LogowanieService();
+            logowanieService.Logowanie(potwierdzenie);
+
+            return potwierdzenie;
+        }
+    }
+}
