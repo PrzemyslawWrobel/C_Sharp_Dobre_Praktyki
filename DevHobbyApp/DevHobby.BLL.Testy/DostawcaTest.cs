96881fcfbe6312f35d0241f0a3f2c93d2aab8f5d
PW <przemek.wrobel@poczta.fm>
2020-03-24 12:38:47 +0100

    dobre metody cd

--- DevHobbyApp/DevHobby.BLL.Testy/DostawcaTest.cs
+++ DevHobbyApp/DevHobby.BLL.Testy/DostawcaTest.cs
@@ -61,14 +61,15 @@ namespace DevHobby.BLL.Testy
             var dostawca = new Dostawca();
             var produkt = new Produkt(1, "Biurko", "Opis");
 
-            var oczekiwana = new WynikOperacji(true, ");
+            var oczekiwana = new WynikOperacji(true, "Zamowienie z dev-hobby.pl\r\nProdukt: Informatyka - 1\r\nIlość: 15");
 
             //ACT wykonaj test
             var actualna = dostawca.ZlozZamowienie(produkt, 15);
 
             // Assert
 
-            Assert.AreEqual(oczekiwana, actualna);
+            Assert.AreEqual(oczekiwana.Sukces, actualna.Sukces);
+            Assert.AreEqual(oczekiwana.Wiadomosc, actualna.Wiadomosc);
         }
 
         [TestMethod]
@@ -102,6 +103,40 @@ namespace DevHobby.BLL.Testy
 
             // Assert.AreEqual(oczekiwana, actualna);
             //  Oczekiwany Wyjątek
+        }
+        [TestMethod]
+        public void ZlozZamowienie_3parametry_Test()
+        {
+            // Arange
+            var dostawca = new Dostawca();
+            var produkt = new Produkt(1, "Biurko", "Opis");
+
+            var oczekiwana = new WynikOperacji(true, "Zamowienie z dev-hobby.pl\r\nProdukt: Informatyka - 1\r\nIlość: 15\r\nData dostawy: 05.05.2020");
+
+            //ACT wykonaj test
+            var actualna = dostawca.ZlozZamowienie(produkt, 15, new DateTimeOffset(2020,5,5,0,0,0, new TimeSpan(8,0,0)));
+
+            // Assert
+
+            Assert.AreEqual(oczekiwana.Sukces, actualna.Sukces);
+            Assert.AreEqual(oczekiwana.Wiadomosc, actualna.Wiadomosc);
+        }
+        [TestMethod]
+        public void ZlozZamowienie_4parametry_Test()
+        {
+            // Arange
+            var dostawca = new Dostawca();
+            var produkt = new Produkt(1, "Biurko", "Opis");
+
+            var oczekiwana = new WynikOperacji(true, "Zamowienie z dev-hobby.pl\r\nProdukt: Informatyka - 1\r\nIlość: 15\r\nData dostawy: 05.05.2020\r\nInstrukcje: Testowe Instrukcje");
+
+            //ACT wykonaj test
+            var actualna = dostawca.ZlozZamowienie(produkt, 15, new DateTimeOffset(2020, 5, 5, 0, 0, 0, new TimeSpan(8, 0, 0)), "Testowe Instrukcje");
+
+            // Assert
+
+            Assert.AreEqual(oczekiwana.Sukces, actualna.Sukces);
+            Assert.AreEqual(oczekiwana.Wiadomosc, actualna.Wiadomosc);
         }
     }
 }
--- DevHobbyApp/DevHobby.BLL/Dostawca.cs
+++ DevHobbyApp/DevHobby.BLL/Dostawca.cs
@@ -40,9 +40,9 @@ namespace DevHobby.BLL
                 throw new ArgumentOutOfRangeException(nameof(ilosc));
 
             var sukces = false;
-            var tekstZamowienia = " zamowienie z dev-hobby.pl" + Environment.NewLine +
-                "Produkt " + produkt.KodProduktu + Environment.NewLine +
-                "Ilość" + ilosc;
+            var tekstZamowienia = "Zamowienie z dev-hobby.pl" + Environment.NewLine +
+                "Produkt: " + produkt.KodProduktu + Environment.NewLine +
+                "Ilość: " + ilosc;
             var emailService = new EmailService();
             var potwierdzenie  = emailService.WyslijWiadomosc("Nowe Zamowienie", tekstZamowienia, this.Email);
 
@@ -51,6 +51,80 @@ namespace DevHobby.BLL
             var wynikiOperacji = new WynikOperacji(sukces, tekstZamowienia);
             return wynikiOperacji;
         }
+        /// <summary>
+        /// Metoda wysyła zamowienie do dostawcy
+        /// </summary>
+        /// <param name="produkt">Produkt do zamowienia</param>
+        /// <param name="ilosc">Ilość produktu do zamowienia</param>
+        /// <param name="data">Data dostawy zamówienia</param>
+        /// <returns></returns>
+        public WynikOperacji ZlozZamowienie(Produkt produkt, int ilosc, DateTimeOffset? data)
+        {
+            if (produkt == null)
+                throw new ArgumentNullException(nameof(produkt));
+            if (ilosc <= 0)
+                throw new ArgumentOutOfRangeException(nameof(ilosc));
+            if (data <= DateTimeOffset.Now)
+                throw new ArgumentOutOfRangeException(nameof(data));
 
+            var sukces = false;
+            var tekstZamowienia = "Zamowienie z dev-hobby.pl" + Environment.NewLine +
+                "Produkt: " + produkt.KodProduktu + Environment.NewLine +
+                "Ilość: " + ilosc;
+
+            if (data.HasValue)
+            {
+                tekstZamowienia += Environment.NewLine + "Data dostawy: " + data.Value.ToString("d");
+            }
+
+            var emailService = new EmailService();
+            var potwierdzenie = emailService.WyslijWiadomosc("Nowe Zamowienie", tekstZamowienia, this.Email);
+
+            if (potwierdzenie.StartsWith("Wiadomość wysłana: "))
+                sukces = true;
+            var wynikiOperacji = new WynikOperacji(sukces, tekstZamowienia);
+            return wynikiOperacji;
+        }
+
+        /// <summary>
+        /// Metoda wysyła zamowienie do dostawcy
+        /// </summary>
+        /// <param name="produkt">Produkt do zamowienia</param>
+        /// <param name="ilosc">Ilość produktu do zamowienia</param>
+        /// <param name="data">Data dostawy zamówienia</param>
+        /// <param name="instrukcje">Instrukcje dostawy</param>
+        /// <returns></returns>
+        public WynikOperacji ZlozZamowienie(Produkt produkt, int ilosc, DateTimeOffset? data, string instrukcje)
+        {
+            if (produkt == null)
+                throw new ArgumentNullException(nameof(produkt));
+            if (ilosc <= 0)
+                throw new ArgumentOutOfRangeException(nameof(ilosc));
+            if (data <= DateTimeOffset.Now)
+                throw new ArgumentOutOfRangeException(nameof(data));
+
+            var sukces = false;
+            var tekstZamowienia = "Zamowienie z dev-hobby.pl" + Environment.NewLine +
+                "Produkt: " + produkt.KodProduktu + Environment.NewLine +
+                "Ilość: " + ilosc;
+
+            if (data.HasValue)
+            {
+                tekstZamowienia += Environment.NewLine + "Data dostawy: " + data.Value.ToString("d");
+            }
+
+            if (!string.IsNullOrWhiteSpace(instrukcje))
+            {
+                tekstZamowienia += Environment.NewLine + "Instrukcje: " + instrukcje;
+            }
+
+            var emailService = new EmailService();
+            var potwierdzenie = emailService.WyslijWiadomosc("Nowe Zamowienie", tekstZamowienia, this.Email);
+
+            if (potwierdzenie.StartsWith("Wiadomość wysłana: "))
+                sukces = true;
+            var wynikiOperacji = new WynikOperacji(sukces, tekstZamowienia);
+            return wynikiOperacji;
+        }
     }
 }
